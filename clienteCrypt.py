7e0bb977fc9afc963770272447745e5760430f8b
Niaev <trutnv@protonmail.com>
2020-08-19 00:49:26 -0300

    Basic connections errors handling

--- clienteCrypt.py
+++ clienteCrypt.py
@@ -1,5 +1,8 @@
+import sys
+
 from socket import socket, AF_INET, SOCK_STREAM
 from Crypto.Cipher import AES
+
 key = "OIESSAEHASENHAOK"
 nkey = len(key)
 aes = AES.new(key, AES.MODE_ECB)
@@ -9,13 +12,21 @@ port = int(input('Server port: '))
 
 while True:
     c = socket(AF_INET, SOCK_STREAM)
-    c.connect((server, port))
-    while True:
-        m = bytes(input("send a message: "), 'utf-8')
-        while len(m) % nkey > 0:     
-            m += b"&"
-        m_crypt = aes.encrypt(m)
-        c.send(m_crypt)
-        r = c.recv(1024)
-        print("received: ", aes.decrypt(r).decode().replace('&',''))
+    try:
+        c.connect((server, port))
+    except ConnectionRefusedError as e:
+        print('\nThis server does not exist.\nEnding connection...')
+        sys.exit()
+    try:
+        while True:
+            m = bytes(input("send a message: "), 'utf-8')
+            while len(m) % nkey > 0:     
+                m += b"&"
+            m_crypt = aes.encrypt(m)
+            c.send(m_crypt)
+            r = c.recv(1024)
+            print("received: ", aes.decrypt(r).decode().replace('&',''))
+    except (ConnectionAbortedError, ConnectionResetError) as e:
+        print('\nIt looks like the server is no longer working\nEnding connection...')
+        sys.exit()
     c.close()
--- serverCrypt.py
+++ serverCrypt.py
@@ -1,5 +1,8 @@
+import sys
+
 from socket import socket, AF_INET, SOCK_STREAM
 from Crypto.Cipher import AES
+
 key = "OIESSAEHASENHAOK"
 nkey = len(key)
 aes = AES.new(key, AES.MODE_ECB)
@@ -8,18 +11,27 @@ server = '127.0.0.1'
 port = int(input('Server port: '))
 
 c = socket(AF_INET, SOCK_STREAM)
-c.bind((server, port))
+try:
+    c.bind((server, port))
+except OSError as e:
+    print('\nYou are trying to use a unavailable port.\nShutting down server...')
+    sys.exit()
 c.listen(2)
 print("Waiting users...")
-while True:
-    conn, cli = c.accept()
-    print("Connected with: ", cli)
+
+try:
     while True:
-        r = conn.recv(1024)
-        print("received: ", aes.decrypt(r).decode().replace('&',''))
-        m = bytes(input("send a message: "), 'utf-8')
-        while len(m) % nkey > 0:
-            m += b"&"
-        m_crypt = aes.encrypt(m)
-        conn.send(m_crypt)
-    conn.close()
+        conn, cli = c.accept()
+        print("Connected with: ", cli)
+        while True:
+            r = conn.recv(1024)
+            print("received: ", aes.decrypt(r).decode().replace('&',''))
+            m = bytes(input("send a message: "), 'utf-8')
+            while len(m) % nkey > 0:
+                m += b"&"
+            m_crypt = aes.encrypt(m)
+            conn.send(m_crypt)
+        conn.close()
+except KeyboardInterrupt as e:
+    print('\nShutting down server...')
+    sys.exit()
